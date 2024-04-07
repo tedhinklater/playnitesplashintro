a8d749caf8619883ae6dffdcc3474a714790b384
tedhinklater <66086488+tedhinklater@users.noreply.github.com>
2024-04-07 21:17:20 +0100

    Add files via upload

---
+++ playnite splash muted.bat
@@ -0,0 +1,2 @@
+start "" "C:\Users\james\AppData\Local\Playnite\Playnite.DesktopApp.exe" --hidesplashscreen
+start "" "C:\Users\james\AppData\Roaming\Playnite\Script\ffplay.exe" -left 0 -top 0 -x 1920 -y 1080 -alwaysontop -noborder -autoexit -loglevel quiet -loop 1 -an "C:\Users\james\AppData\Roaming\Playnite\Script\splash.mp4" 2>NUL
