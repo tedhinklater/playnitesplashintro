0ecee07b79693a29d06fe95a09f0f34b8b74088e
tedhinklater <66086488+tedhinklater@users.noreply.github.com>
2024-04-09 01:33:54 +0100

    Update playnite splash.bat

--- playnite splash.bat
+++ playnite splash.bat
@@ -1,2 +1,2 @@
-start "" "C:\Users\user\AppData\Local\Playnite\Playnite.FullscreenApp.exe" --hidesplashscreen
-start "" "C:\Users\user\AppData\Roaming\Playnite\Script\ffplay.exe" -left 0 -top 0 -x 1920 -y 1080 -alwaysontop -noborder -autoexit -loglevel quiet -loop 1 "C:\Users\user\AppData\Roaming\Playnite\Script\splash.mp4" 2>NUL
+start "" "C:\Users\YOURUSERNAME\AppData\Local\Playnite\Playnite.FullscreenApp.exe" --hidesplashscreen
+start "" "C:\Users\YOURUSERNAME\Documents\FFMPEG\ffplay.exe" -left 0 -top 0 -x 1920 -y 1080 -alwaysontop -noborder -autoexit -loglevel quiet -loop 1 "C:\Users\YOURUSERNAME\Documents\FFMPEG\splash.mp4" 2>NUL
