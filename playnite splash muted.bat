c9553b71c53957c06c87ba104f500349cdbd7b24
tedhinklater <66086488+tedhinklater@users.noreply.github.com>
2024-04-09 02:07:49 +0100

    Update playnite splash muted.bat

--- playnite splash muted.bat
+++ playnite splash muted.bat
@@ -1,2 +1,2 @@
-start "" "C:\Users\james\AppData\Local\Playnite\Playnite.DesktopApp.exe" --hidesplashscreen
-start "" "C:\Users\james\AppData\Roaming\Playnite\Script\ffplay.exe" -left 0 -top 0 -x 1920 -y 1080 -alwaysontop -noborder -autoexit -loglevel quiet -loop 1 -an "C:\Users\james\AppData\Roaming\Playnite\Script\splash.mp4" 2>NUL
+start "" "C:\Users\YOURUSERNAME\AppData\Local\Playnite\Playnite.FullScreenApp.exe" --hidesplashscreen
+start "" "C:\Users\YOURUSERNAME\Documents\FFMPEG\ffplay.exe" -left 0 -top 0 -x 1920 -y 1080 -alwaysontop -noborder -autoexit -loglevel quiet -loop 1 -an "C:\Users\YOURUSERNAME\Documents\FFMPEG\splash.mp4" 2>NUL
